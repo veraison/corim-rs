539c999b4ceffec3f30abdc43cf0895d2272b33c
setrofim <setrofim@gmail.com>
2025-07-02 11:47:33 +0100

    ConciseTlTag human-readable serialization
    Implement serialization for ConciseTlTag that uses string field names
    for human-readable formats, and integer keys otherwise.
    
    Also, add a Builder to simplify construction of valid ConciseTlTag's.
    
    Signed-off-by: setrofim <setrofim@gmail.com>

--- src/error/cotl.rs
+++ src/error/cotl.rs
@@ -2,7 +2,19 @@
 
 #[derive(Debug)]
 pub enum CotlError {
+    UnsetMandatoryField(String, String),
+    Custom(String),
     Unknown,
+}
+
+impl CotlError {
+    pub fn unset_mandatory_field<D: std::fmt::Display>(object: D, field: D) -> Self {
+        CotlError::UnsetMandatoryField(object.to_string(), field.to_string())
+    }
+
+    pub fn custom<D: std::fmt::Display>(message: D) -> Self {
+        CotlError::Custom(message.to_string())
+    }
 }
 
 impl std::error::Error for CotlError {}
@@ -10,6 +22,10 @@ impl std::error::Error for CotlError {}
 impl std::fmt::Display for CotlError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
+            Self::UnsetMandatoryField(object, field) => {
+                write!(f, "{object} field(s) {field} must be set")
+            }
+            Self::Custom(message) => f.write_str(message.as_str()),
             Self::Unknown => write!(f, "unknown CotlError encountered"),
         }
     }
